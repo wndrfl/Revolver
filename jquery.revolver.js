d3bca04926ba0ae1da9689762b330c1eea91dd26
Johnnie Munger <johnnietheblack@gmail.com>
2013-08-15 15:09:21 -0700

    upgraded javascript

--- jquery.revolver.js
+++ jquery.revolver.js
@@ -20,7 +20,6 @@
 			inactiveClass	: 'revolver-slide-inactive',
 			isAnimating		: false,
 			isPaused		: false,
-			nextSlide		: null,
 			prevSlide		: null,
 			reverseOnce		: false,
 			timerStart		: 0,
@@ -48,18 +47,10 @@
 		var $children = $wrapper.children(settings.childrenEls);
 		
 		// my private parts
+		
 		var determineNextAction = function() {
 			if(!vars.isPaused) {
-				$revolver.start();
-			}
-		}
-		
-		var hidePreviousSlide = function() {
-			if(settings.hideInactiveSlides) {
-				$children
-					.eq(vars.prevSlide-1)
-					.removeClass(vars.inactiveClass)
-					.animate({'opacity':'0'},300);
+				$revolver.startRevolver();
 			}
 		}
 		
@@ -101,7 +92,7 @@
 			if(settings.manualButton !== null) {
 				$(settings.manualButton).click(function(e) {
 					e.preventDefault();
-					var slide = $(this).attr('data-slide');
+					var slide = $(this).attr('data-slide')*1;
 					if(slide) {
 						showSlide(slide);
 					}
@@ -137,10 +128,10 @@
 			
 			if(settings.pauseOnHover) {
 				$parent.hover(function() {
-					$revolver.pause();
+					$revolver.pauseRevolver();
 				},
 				function() {
-					$revolver.start();
+					$revolver.startRevolver();
 				});
 			}
 		}
@@ -157,177 +148,183 @@
 		}
 		
 		var showSlide = function(i) {
-			trace('Show slide '+i);
 			
-			// callback
-			settings.beforeChange.call(this,vars);
+			trace('Show slide '+i);
 
-			// stop timer
-			$revolver.stop();
+			// cleanup previous commands first
+			$revolver.stopRevolver();
+			$revolver.cleanupRevolver();
 			
 			// lock
 			vars.isAnimating = true;
-
-			// queue next
-			vars.nextSlide = i;
 			
-			// fade in
-			if(settings.transition == 'fadeIn') {
+			vars.prevSlide = vars.currSlide;
+			vars.currSlide = i;
+			
+			// no transition if the same slide
+			if(vars.currSlide != vars.prevSlide) {
 				
-				$children.eq(vars.currSlide-1)
-					.removeClass(vars.activeClass)
-					.addClass(vars.inactiveClass).css({
-					'z-index'	: '0'
-				});
-				vars.prevSlide = vars.currSlide;
+				// before change callback
+				settings.beforeChange.call(this,vars);
 				
-				var startFadeParams = {
-					opacity		: 0,
-					'z-index'	: '100'
-				};
-				if(settings.transitionDir == 'right') {
-					startFadeParams.left = '+=50';
+				// fade in
+				if(settings.transition == 'fadeIn') {
 					
-				} else if(settings.transitionDir == 'left') {
-					startFadeParams.left = '-=50';
-					
-				}
+					// prepare prev to hide
+					$children.eq(vars.prevSlide-1)
+						.removeClass(vars.activeClass)
+						.addClass(vars.inactiveClass)
+						.css({'z-index':99});
+			
+					// how to fade in new slide
+					var startRevolverFadeParams = {
+						opacity		: 0,
+						'z-index'	: '100'
+					};
+					if(settings.transitionDir == 'right') {
+						startRevolverFadeParams.left = '+=50';
+					} else if(settings.transitionDir == 'left') {
+						startRevolverFadeParams.left = '-=50';
+					}
+			
+					// animate slide in
+					$children
+						.eq(vars.currSlide-1)
+						.removeClass(vars.inactiveClass)
+						.addClass(vars.activeClass)
+						.css(startRevolverFadeParams)
+						.animate({
+							opacity		: 1,
+							top			: 0,
+							left 		: 0
+						},settings.transitionDuration,function() {
+							vars.isAnimating = false;
+							determineNextAction();
+							settings.afterChange.call(this,vars);
+						});
 				
-				$children
-					.eq(vars.nextSlide-1)
-					.addClass(vars.activeClass)
-					.css(startFadeParams)
-					.animate({
-						opacity		: 1,
-						top			: 0,
-						left 		: 0
-					},settings.transitionDuration,function() {
-						vars.currSlide = vars.nextSlide;
-						hidePreviousSlide();
-						determineNextAction();
-						vars.isAnimating = false;
-						
-						// callback
-						settings.afterChange.call(this,vars);
+			
+				// fade out
+				} else if(settings.transition == 'fadeOut') {
+				
+					// prepare prev slide
+					$children.eq(vars.prevSlide-1)
+						.removeClass(vars.activeClass)
+						.addClass(vars.inactiveClass)
+						.css({
+							'z-index'	: 101
+						});
+				
+					// prepare curr slide
+					$children
+						.eq(vars.currSlide-1)
+						.removeClass(vars.inactiveClass)
+						.addClass(vars.activeClass)
+						.css({
+							opacity		: 1,
+							top			: 0,
+							left 		: 0,
+							'z-index'	: 100
+						});
+				
+					// fade out params
+					var fadeParams = {
+						opacity		: 0
+					};
+					if(settings.transitionDir == 'right') {
+						fadeParams.left = '+=50'
+					
+					} else if(settings.transitionDir == 'left') {
+						fadeParams.left = '-=50'
+					}
+				
+					$children
+						.eq(vars.prevSlide-1)
+						.animate(fadeParams,settings.transitionDuration,function() {
+							determineNextAction();
+							vars.isAnimating = false;
+							settings.afterChange.call(this,vars);
 					});
 				
-			
-			// fade out
-			} else if(settings.transition == 'fadeOut') {
+				} else if(settings.transition == 'slide') {
 				
-				$children.eq(vars.currSlide-1)
-					.removeClass(vars.activeClass)
-					.addClass(vars.inactiveClass).css({
-					'z-index'	: '101'
-				});
-				vars.prevSlide = vars.currSlide;
+					var nextStartParams = {};
+					var nextEndParams = {
+						opacity	: 1
+					};
+					var prevEndParams = {};
 				
-				$children.eq(vars.nextSlide-1).css({
-					opacity		: 1,
-					top			: 0,
-					left 		: 0
-				});
+					// startRevolver w/ opacity?
+					if(!settings.hideInactiveSlides) {
+						nextStartParams.opacity = 1;
+					}else{
+						prevEndParams.opacity = 0;
+					}
 				
-				// setup params
-				var fadeParams = {
-					opacity		: 0
-				};
-				if(settings.transitionDir == 'right') {
-					fadeParams.left = '+=50'
-					
-				} else if(settings.transitionDir == 'left') {
-					fadeParams.left = '-=50'
-				}
-				
-				$children
-					.eq(vars.prevSlide-1)
-					.animate(fadeParams,settings.transitionDuration,function() {
-						vars.currSlide = vars.nextSlide;
-						hidePreviousSlide();
-						determineNextAction();
-						vars.isAnimating = false;
-						
-						// callback
-						settings.afterChange.call(this,vars);
-				});
-				
-			} else if(settings.transition == 'slide') {
-				
-				vars.prevSlide = vars.currSlide;
-				
-				var nextStartParams = {};
-				var nextEndParams = {
-					opacity	: 1
-				};
-				var prevEndParams = {};
-				
-				// start w/ opacity?
-				if(!settings.hideInactiveSlides) {
-					nextStartParams.opacity = 1;
-				}else{
-					prevEndParams.opacity = 0;
-				}
-				
-				// default is to slide from right
-				nextStartParams.left = '100%';
-				nextEndParams.left = 0;
-				prevEndParams.left = '-100%';
-				
-				// slide from right?
-				if(settings.transitionDir == 'left' && vars.reverseOnce) {
+					// default is to slide from right
 					nextStartParams.left = '100%';
-					nextStartParams.top = 0;
 					nextEndParams.left = 0;
 					prevEndParams.left = '-100%';
-					prevEndParams.top = 0;
+				
+					// slide from right?
+					if(settings.transitionDir == 'left' && vars.reverseOnce) {
+						nextStartParams.left = '100%';
+						nextStartParams.top = 0;
+						nextEndParams.left = 0;
+						prevEndParams.left = '-100%';
+						prevEndParams.top = 0;
 						
-				// slide from left?
-				}else if((settings.transitionDir == 'left' && !vars.reverseOnce)
-					|| (settings.transitionDir == 'right' && vars.reverseOnce)) {
-					nextStartParams.top = 0;
-					nextStartParams.left = '-100%';
-					nextEndParams.left = 0;
-					prevEndParams.top = 0;
-					prevEndParams.left = '100%';
+					// slide from left?
+					}else if((settings.transitionDir == 'left' && !vars.reverseOnce)
+						|| (settings.transitionDir == 'right' && vars.reverseOnce)) {
+						nextStartParams.top = 0;
+						nextStartParams.left = '-100%';
+						nextEndParams.left = 0;
+						prevEndParams.top = 0;
+						prevEndParams.left = '100%';
 				
-				// slide from top?
-				}else if((settings.transitionDir == 'top' && !vars.reverseOnce)
-					|| (settings.transitionDir == 'bottom' && vars.reverseOnce)) {
-					nextStartParams.left = 0;
-					nextStartParams.top = '-100%';
-					nextEndParams.top = 0;
-					prevEndParams.left = 0;
-					prevEndParams.top = '100%';
+					// slide from top?
+					}else if((settings.transitionDir == 'top' && !vars.reverseOnce)
+						|| (settings.transitionDir == 'bottom' && vars.reverseOnce)) {
+						nextStartParams.left = 0;
+						nextStartParams.top = '-100%';
+						nextEndParams.top = 0;
+						prevEndParams.left = 0;
+						prevEndParams.top = '100%';
 
-				// slide from bottom?
-				}else if((settings.transitionDir == 'bottom' && !vars.reverseOnce)
-					|| (settings.transitionDir == 'top' && vars.reverseOnce)) {
-					nextStartParams.left = 0;
-					nextStartParams.top = '100%';
-					nextEndParams.top = 0;
-					prevEndParams.left = 0;
-					prevEndParams.top = '-100%';
+					// slide from bottom?
+					}else if((settings.transitionDir == 'bottom' && !vars.reverseOnce)
+						|| (settings.transitionDir == 'top' && vars.reverseOnce)) {
+						nextStartParams.left = 0;
+						nextStartParams.top = '100%';
+						nextEndParams.top = 0;
+						prevEndParams.left = 0;
+						prevEndParams.top = '-100%';
+					}
+				
+					$children.eq(vars.prevSlide-1)
+						.removeClass(vars.activeClass)
+						.addClass(vars.inactiveClass);
+				
+					// animate next slide
+					$children.eq(vars.currSlide-1)
+						.removeClass(vars.inactiveClass)
+						.addClass(vars.activeClass)
+						.css(nextStartParams)
+						.animate(nextEndParams,settings.transitionDuration,function() {
+							determineNextAction();
+							vars.isAnimating = false;
+							settings.afterChange.call(this,vars);
+					});
+				
+					$children.eq(vars.prevSlide-1).animate(prevEndParams,settings.transitionDuration);
 				}
-				
-				$children.eq(vars.currSlide-1)
-					.removeClass(vars.activeClass)
-					.addClass(vars.inactiveClass);
-				
-				// animate next slide
-				$children.eq(vars.nextSlide-1)
-					.removeClass(vars.inactiveClass)
-					.addClass(vars.activeClass)
-					.css(nextStartParams)
-					.animate(nextEndParams,settings.transitionDuration,function() {
-						vars.currSlide = vars.nextSlide;
-						determineNextAction();
-						vars.isAnimating = false;
-					
-						// callback
-						settings.afterChange.call(this,vars);
-				});
-				
-				$children.eq(vars.prevSlide-1).animate(prevEndParams,settings.transitionDuration);
+			
+			// showing the same slide - no transition	
+			}else{	
+				vars.isAnimating = false;
+				determineNextAction();
+				settings.afterChange.call(this,vars);
 			}
 			
 			// reset reverse-ness
@@ -340,13 +337,22 @@
 			}
         }
 		
-		this.pause = function() {
+		this.cleanupRevolver = function() {
+			$children.each(function() {
+				if($(this).hasClass(vars.inactiveClass)) {
+					$(this)
+						.css({'opacity':'0','z-index':'0'});
+				}
+			});
+		}
+		
+		this.pauseRevolver = function() {
 			trace('Pause');
-			$revolver.stop();
+			$revolver.stopRevolver();
 			vars.isPaused = true;
 		}
 		
-		this.start = function() {
+		this.startRevolver = function() {
 			trace('Start');
 			vars.isPaused = false;
 			
@@ -355,15 +361,16 @@
 			},settings.rotationDelay);
 		}
 		
-		this.stop = function() {
+		this.stopRevolver = function() {
 			trace('Stop');
-			window.clearTimeout(vars.timeout);
+			$children.stop(true);
+			clearTimeout(vars.timeout);
 		}
 
 		setup();
 		
 		if(settings.autoplay) {
-			$revolver.start();
+			$revolver.startRevolver();
 		}
 		
 	}
